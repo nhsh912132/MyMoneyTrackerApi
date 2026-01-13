76c8644c854e0fdb32aac138ec37fe10fe4b7a10
nhsh912132 <35324750+nhsh912132@users.noreply.github.com>
2026-01-13 17:03:04 +0800

    Update config.php

--- auto_function/config.php
+++ auto_function/config.php
@@ -1,11 +1,8 @@
 <?php
 	$db_server = "localhost";
 	$db_name = "MyMoze";
-	$db_user = "fuyuanservicedb";
-	$db_passwd = "Jhsh096167#";
-
-	// $db_user = "mozedbuser";
-	// $db_passwd = "B@q2H1t01PUj-gjz";
+	$db_user = "";
+	$db_passwd = "";//主要是提供php程式碼，不提供連線資料庫的帳號密碼
 
 
 	date_default_timezone_set("Asia/Taipei");
