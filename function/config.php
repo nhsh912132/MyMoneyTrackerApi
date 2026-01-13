0c8bbc3f83a13b915e67add6b13ecbb5fe3e5815
nhsh912132 <35324750+nhsh912132@users.noreply.github.com>
2026-01-13 17:03:39 +0800

    Update config.php

--- function/config.php
+++ function/config.php
@@ -2,7 +2,7 @@
 	$db_server = "localhost";
 	$db_name = "MyMoze";
 	$db_user = "";
-	$db_passwd = "";//主要是提供php程式碼怎麼寫的，因此不提供登入的帳號密碼
+	$db_passwd = "";//主要是提供php程式碼，不提供連線資料庫的帳號密碼
 
 
 	date_default_timezone_set("Asia/Taipei");
