2bf8cb0b41dbc395fade708e8e77f611bfebe505
nhsh912132 <35324750+nhsh912132@users.noreply.github.com>
2026-01-19 10:40:38 +0800

    Delete function directory

--- function/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db_server = "localhost";
-	$db_name = "MyMoze";
-	$db_user = "";
-	$db_passwd = "";//主要是提供php程式碼，不提供連線資料庫的帳號密碼
-
-
-	date_default_timezone_set("Asia/Taipei");
-?>
--- function/function/testPWhash.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo password_hash("jhsh12345678",PASSWORD_DEFAULT);
-
-?>
